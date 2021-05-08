42d082631763d75a1cce2dbfef2def733783f278
Ashley Pean <pean.ashley@gmail.com>
2021-05-08 17:39:12 -0400

    remove unused variables

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -13,7 +13,7 @@ const styles = {
     width: '100vw',
     border: '1px solid purple',
     display: 'grid', 
-    gridTemplateRows: 'repeat(7, 100/9)',
+    gridTemplateRows: 'repeat(7, 100/7)',
     gridTemplateColumns: 'repeat(6, 100/6)',
     gridTemplateAreas: `
       "nav nav nav nav nav nav nav nav"
