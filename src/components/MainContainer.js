f0c725ddc85eca254a032d9ee716f7c6e6df9778
Ashley Pean <pean.ashley@gmail.com>
2021-05-08 17:26:55 -0400

    fixed tab sizing

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,6 +3,7 @@ import {
   Container,
 } from '@material-ui/core'
 import MainContainerProvider from '../Global/context/MainContainerContext';
+import TabContainer from './Tab/TabContainer';
 
 const styles = {
   container: {
@@ -12,11 +13,12 @@ const styles = {
     width: '100vw',
     border: '1px solid purple',
     display: 'grid', 
-    gridTemplateRows: 'repeat(8, 100/8)',
+    gridTemplateRows: 'repeat(7, 100/9)',
     gridTemplateColumns: 'repeat(6, 100/6)',
     gridTemplateAreas: `
       "nav nav nav nav nav nav nav nav"
       "sidebar tabs tabs tabs tabs tabs tabs tabs"
+      "sidebar . . . . . . ."
       "sidebar windows windows windows windows windows windows windows"
       "sidebar windows windows windows windows windows windows windows"
       "sidebar windows windows windows windows windows windows windows"
@@ -29,6 +31,7 @@ const styles = {
 const MainContainer = () => (
   <MainContainerProvider>
     <Container style={styles.container}>
+      <TabContainer />
     </Container>
   </MainContainerProvider>
 )
