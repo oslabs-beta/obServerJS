9e1bd0a424370dec6c915b13f96964684c21ee5e
Ashley Pean <pean.ashley@gmail.com>
2021-05-18 23:04:53 -0400

    redo basic layout for response container

--- src/components/Response/Variables.js
+++ src/components/Response/Variables.js
@@ -1,8 +1,5 @@
 import React, { useContext } from 'react'
-import {
-  Box,
-  Container,
-} from '@material-ui/core'
+import { Container } from '@material-ui/core'
 import { MainContainerContext } from '../../Global/context/MainContainerContext'
 
 const styles = {
@@ -23,17 +20,15 @@ const Variables = () => {
     } 
   } = useContext(MainContainerContext)
 
-  console.log((Object.values(activeMiddleware.variables)).length )
-
   return (
     <Container style={styles.container}>
       <h1>Variables</h1>
-      {Object.keys(activeMiddleware.variables).length 
+      {Object.keys(activeMiddleware?.variables)?.length 
       ? activeMiddleware.variables.map((variableObj) => {
         return Object.keys(variableObj).map((variable) => (
           <p>{`${variable}: ${variableObj[variable]}`}</p>
         ))
-      }): 'No variables available'}
+      }): <p>No variables available</p>}
     </Container>
   )
 }
