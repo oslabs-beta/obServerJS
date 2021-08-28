0927a28c6bc3170b296bdcbb4fdb7fd19b8ae59b
Eric Wilding <ewilding8@gmail.com>
2021-08-27 21:22:23 -0500

    Update response obj visuals

--- app/src/components/Response/Middleware/ResponseObject.jsx
+++ app/src/components/Response/Middleware/ResponseObject.jsx
@@ -1,8 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {
   makeStyles,
   Paper,
+  Container,
 } from '@material-ui/core';
+import { format as prettyFormat } from 'pretty-format'; // ES2015 modules
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atelierCaveDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { MainContainerContext } from '../../../Global/context/MainContainerContext';
 
 const useStyles = makeStyles({
@@ -17,6 +21,7 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     height: '100%',
     overflow: 'auto',
+    alignItems: 'center',
     '&::-webkit-scrollbar': {
       width: '10px',
       height: '10px',
@@ -41,27 +46,50 @@ const useStyles = makeStyles({
     color: 'white',
     fontSize: 14,
   },
-  keys: {
-    minWidth: '100px',
-    maxWidth: '200px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignSelf: 'start',
-    color: 'green',
-  },
-  values: {
-    minWidth: '100px',
-    maxWidth: '300px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignSelf: 'start',
-    margin: '0 5rem',
+  button: {
+    background: 'darkgrey',
+    border: 'none',
     color: 'white',
-    textAlign: 'left',
+    height: 20,
+    width: 20,
+    margin: '0 5px',
+    outline: 'none',
   },
   curlyBrace: {
     alignSelf: 'start',
     color: 'yellow',
+  },
+  codeContainer: {
+    minWidth: 300,
+    minHeight: 300,
+    maxWidth: '100%',
+    maxHeight: '97%',
+    overflow: 'none',
+  },
+  code: {
+    alignContent: 'start',
+    height: 'inherit',
+    width: 'inherit',
+    minHeight: 'inherit',
+    maxHeight: 'inherit',
+    minWidth: 'inherit',
+    maxWidth: 'inherit',
+    overflow: 'auto',
+    resize: 'both',
+    borderRadius: '8px',
+    fontSize: (props) => props?.fontSize || 10,
+    textAlign: 'left',
+    '&::-webkit-scrollbar': {
+      width: '10px',
+      height: '10px',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'gray',
+      borderRadius: '10px',
+    },
+    '&::-webkit-resizer': {
+      background: 'gray',
+    },
   },
 });
 
@@ -89,71 +117,43 @@ const PopulatedResponseObject = () => {
   } = useContext(MainContainerContext);
 
   const currentTab = allTabs[currentTabIdx];
-  const styles = useStyles();
-
-  const jsonparse = (jsonObj) => {
-    if (Object.keys(jsonObj).length === 0 || jsonObj === null) return <p />;
-    const keyValPairs = [];
-
-    const obj = JSON.parse(jsonObj);
-
-    // We don't exactly know the format of the data being returned,
-    // so we have to account for many cases
-    // Let's assume for now there aren't nested, but that case will need to be handled.
-    // We can also optimize code reuse here, and
-    // the physical display of the objects should ideally be cleaned up too.
-
-    // Array of objects:
-    if (Object.prototype.toString.call(obj) === '[object Array]') {
-      obj.forEach((el) => {
-        keyValPairs.push(<p className={styles.curlyBrace}>{'{'}</p>);
-
-        Object.keys(el).forEach((key) => {
-          keyValPairs.push(
-            <code className={styles.keys}>
-              {key}
-              {' '}
-              :
-            </code>,
-            <code className={styles.values}>
-              {' '}
-              {el[key]}
-            </code>,
-          );
-        });
-
-        keyValPairs.push(<p className={styles.curlyBrace}>{'}'}</p>);
-      });
-    } else if (Object.prototype.toString.call(obj) === '[object Object]') {
-    // Single Object:
-      keyValPairs.push(<p className={styles.curlyBrace}>{'{'}</p>);
-      Object.keys(obj).forEach((key) => {
-        keyValPairs.push(
-          <code className={styles.keys}>
-            {key}
-            {' '}
-            :
-          </code>,
-          <code className={styles.values}>
-            {' '}
-            {obj[key]}
-          </code>,
-        );
-      });
-      keyValPairs.push(<p className={styles.curlyBrace}>{'}'}</p>);
-    }
-
-    return keyValPairs;
-  };
+  const [fontSize, setFontSize] = useState(10);
+  const styles = useStyles({ fontSize });
 
   return (
     <Paper elevation={3} className={styles.container}>
       <h1 className={styles.title}>
         Response
       </h1>
-      {
-        jsonparse(currentTab.response)
-      }
+
+      <Container className={styles.codeContainer}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => setFontSize((prev) => prev - 2)}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => setFontSize((prev) => prev + 2)}
+        >
+          +
+        </button>
+        <SyntaxHighlighter
+          language="javascript"
+          showLineNumbers
+          className={styles.code}
+          style={atelierCaveDark}
+          wrapLongLines
+          wrapLines
+        >
+          {
+            prettyFormat(JSON.parse(currentTab.response))
+          }
+        </SyntaxHighlighter>
+      </Container>
     </Paper>
   );
 };
